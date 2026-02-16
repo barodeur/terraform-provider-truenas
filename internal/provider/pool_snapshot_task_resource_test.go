44ff73c0e5812d1ed6daa54713fb05a7dc972a75
Paul Chobert <paul@chobert.fr>
2026-02-16 22:07:13 +0100

    test: add coverage for all fields, exclude list, and partial schedule

--- internal/provider/pool_snapshot_task_resource_test.go
+++ internal/provider/pool_snapshot_task_resource_test.go
@@ -113,6 +113,65 @@ func TestAccPoolSnapshotTaskResource_update(t *testing.T) {
 	})
 }
 
+func TestAccPoolSnapshotTaskResource_allFields(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckPoolSnapshotTaskDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPoolSnapshotTaskResourceConfig_allFields(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("truenas_pool_snapshot_task.test", "id"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "dataset", "tank/snap-test"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "recursive", "true"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "lifetime_value", "7"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "lifetime_unit", "DAY"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "enabled", "true"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "naming_schema", "daily-%Y-%m-%d_%H-%M"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "allow_empty", "false"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "exclude.#", "1"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "exclude.0", "tank/snap-test/child"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.minute", "15"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.hour", "3"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.dom", "1"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.month", "*"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.dow", "*"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.begin", "01:00"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.end", "05:00"),
+				),
+			},
+			{
+				ResourceName:      "truenas_pool_snapshot_task.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccPoolSnapshotTaskResource_partialSchedule(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckPoolSnapshotTaskDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPoolSnapshotTaskResourceConfig_partialSchedule(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.minute", "30"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.hour", "6"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.dom", "*"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.month", "*"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.dow", "*"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.begin", "00:00"),
+					resource.TestCheckResourceAttr("truenas_pool_snapshot_task.test", "schedule.end", "23:59"),
+				),
+			},
+		},
+	})
+}
+
 func testAccPoolSnapshotTaskResourceConfig_basic() string {
 	return testAccProviderConfig() + `
 resource "truenas_pool_dataset" "snap_test" {
@@ -144,6 +203,54 @@ resource "truenas_pool_snapshot_task" "test" {
     dow    = "*"
     begin  = "00:00"
     end    = "23:59"
+  }
+}
+`
+}
+
+func testAccPoolSnapshotTaskResourceConfig_allFields() string {
+	return testAccProviderConfig() + `
+resource "truenas_pool_dataset" "snap_test" {
+  name = "tank/snap-test"
+}
+
+resource "truenas_pool_dataset" "snap_test_child" {
+  name = "tank/snap-test/child"
+}
+
+resource "truenas_pool_snapshot_task" "test" {
+  dataset        = truenas_pool_dataset.snap_test.name
+  recursive      = true
+  lifetime_value = 7
+  lifetime_unit  = "DAY"
+  enabled        = true
+  exclude        = [truenas_pool_dataset.snap_test_child.name]
+  naming_schema  = "daily-%Y-%m-%d_%H-%M"
+  allow_empty    = false
+  schedule = {
+    minute = "15"
+    hour   = "3"
+    dom    = "1"
+    month  = "*"
+    dow    = "*"
+    begin  = "01:00"
+    end    = "05:00"
+  }
+}
+`
+}
+
+func testAccPoolSnapshotTaskResourceConfig_partialSchedule() string {
+	return testAccProviderConfig() + `
+resource "truenas_pool_dataset" "snap_test" {
+  name = "tank/snap-test"
+}
+
+resource "truenas_pool_snapshot_task" "test" {
+  dataset = truenas_pool_dataset.snap_test.name
+  schedule = {
+    minute = "30"
+    hour   = "6"
   }
 }
 `
